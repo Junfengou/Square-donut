ad786c02899ec536202f8cb47724d44deba4621b
Junfeng ou <junfengou@gmail.com>
2020-08-19 09:20:25 -0500

    styling InfoBoxes component

--- src/Component/Info.js
+++ src/Component/Info.js
@@ -5,6 +5,15 @@ function Info() {
   return (
     <div className="info">
       <h3>This is the info boxes page</h3>
+      <div className="info__container">
+        <div className="info__card">
+          <h4>Box one</h4>
+
+          <h4>Box two</h4>
+
+          <h4>Box three</h4>
+        </div>
+      </div>
     </div>
   );
 }
