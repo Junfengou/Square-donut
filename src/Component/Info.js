ac9dbb4e6876ce7a683889caf5cf3639da32c1c5
Junfeng ou <junfengou@gmail.com>
2020-08-19 09:56:18 -0500

    Need more stlying

--- src/Component/Info.js
+++ src/Component/Info.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Info.css";
+import DonutThree from "../pictures/DonutThree.jpg";
 
 function Info() {
   return (
@@ -7,11 +8,19 @@ function Info() {
       <h3>This is the info boxes page</h3>
       <div className="info__container">
         <div className="info__card">
-          <h4>Box one</h4>
-
-          <h4>Box two</h4>
-
-          <h4>Box three</h4>
+          <div className="info__img">
+            <img src={DonutThree} />
+          </div>
+          <div className="info__content">
+            <h2>NEW DONUT SHOP!</h2>
+            <p>
+              Whether you’re nearby Square Donut in West Omaha or you drive a
+              few minutes, we’re honored to be a part of your morning routine.
+              Create memories with the family or be the hero at the office.
+              Square Donut has handcrafted donuts to make your heart (and mouth)
+              sing.
+            </p>
+          </div>
         </div>
       </div>
     </div>
