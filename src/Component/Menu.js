1abde5849f99e32b82e7372c4ff52c71079e006d
Junfeng ou <junfengou@gmail.com>
2020-08-20 15:51:43 -0500

    Menu page is done

--- src/Component/Menu.js
+++ src/Component/Menu.js
@@ -1,50 +1,128 @@
 import React from "react";
-import DonutThree from "../pictures/DonutThree.jpg";
-import DonutTwo from "../pictures/DonutTwo.jpg";
-import Logo from "../pictures/logo2.png";
+import Menu_logo from "../pictures/menu.png";
+import Donuts from "../logos/Donut_cakes.png";
+import Fritters from "../logos/Filled_fritters.png";
+import Vegan from "../logos/Vegan.png";
+import Drinks from "../logos/Drinks.png";
+import Colored from "../pictures/colored.png";
+import Half_Colored from "../pictures/half-colored.png";
 import "./Menu.css";
 
 function Menu() {
   return (
     <div className="menu">
+      <div className="menu__logo">
+        <img src={Colored} className="menu__logo_colored" />
+        <img src={Menu_logo} className="menu__logo_logo" />
+        <img src={Half_Colored} className="menu__logo_colored" />
+      </div>
       <div className="menu__container">
         <div className="menu__card">
           <div className="menu__img">
-            <img src={Logo} />
+            <img src={Donuts} />
           </div>
+
           <div className="menu__content">
-            <h3>Donuts / Cakes</h3>
-            <p>Donuts and cakes</p>
+            <div className="menu__content__wrapper">
+              <div className="menu__content__item">
+                <h4>Raised Donuts</h4>
+                <h6>Glazed</h6>
+              </div>
+
+              <div className="menu__content__item">
+                <h4>Raised Topped</h4>
+                <h6>
+                  Chocolate | Vanilla | Maple | Cinnamon Sugar | Chocolate
+                  Peanut (SP) | Maple Bacon (SP)
+                </h6>
+              </div>
+
+              <div className="menu__content__item">
+                <h4>Cake Donuts</h4>
+                <h6>
+                  Original glazed & plain (Original is SC Old Fashion cake
+                  donut)
+                </h6>
+              </div>
+            </div>
           </div>
         </div>
 
         <div className="menu__card">
           <div className="menu__img">
-            <img src={Logo} />
+            <img src={Fritters} />
           </div>
+
           <div className="menu__content">
-            <h3>Filled / Fritters</h3>
-            <p>Filled donuts</p>
+            <div className="menu__content__wrapper">
+              <div className="menu__content__item">
+                <h4>Filled (Specialties)</h4>
+                <h6>
+                  Powdered Sugar + Fruit of the Day Square John (Bavarian
+                  Filled, Chocolate Topped) Square Jane (Cream Filled, Chocolate
+                  Topped)
+                </h6>
+              </div>
+
+              <div className="menu__content__item">
+                <h4>Fritters (Specialties)</h4>
+                <h6>Apple | Caramel Apple</h6>
+              </div>
+
+              <div className="menu__content__item">
+                <h4>Topped Original Cake</h4>
+                <h6>Chocolate | Vanilla | Maple</h6>
+              </div>
+            </div>
           </div>
         </div>
 
         <div className="menu__card">
           <div className="menu__img">
-            <img src={Logo} />
+            <img src={Vegan} />
           </div>
+
           <div className="menu__content">
-            <h3>Vegan options</h3>
-            <p>Veggies</p>
+            <div className="menu__content__wrapper">
+              <div className="menu__content__item">
+                <h4>CAKE</h4>
+                <h6>GF Glazed | GF Chocolate Topped | GF Vanilla Topped</h6>
+              </div>
+
+              <div className="menu__content__item">
+                <h4>RAISED OR CAKE (One variety offered daily)</h4>
+                <h6>Vegan Chocolate</h6>
+              </div>
+
+              <div className="menu__content__item">
+                <h4>RAISED OR CAKE (One variety offered daily)</h4>
+                <h6>Vanilla Topped</h6>
+              </div>
+            </div>
           </div>
         </div>
 
         <div className="menu__card">
           <div className="menu__img">
-            <img src={Logo} />
+            <img src={Drinks} />
           </div>
           <div className="menu__content">
-            <h3>Drinks</h3>
-            <p>Hot / Cold</p>
+            <div className="menu__content__wrapper">
+              <div className="menu__content__item">
+                <h4>HOT</h4>
+                <h6>Coffee (provided by Hardy Coffee Co)</h6>
+              </div>
+
+              <div className="menu__content__item">
+                <h4>COLD</h4>
+                <h6>Bottled Water | Pop | Iced Tea | Gatorade | Sobe Water</h6>
+              </div>
+
+              <div className="menu__content__item">
+                <h4>MILK / JUICES</h4>
+                <h6>Premium Orange Juice | Milk | Muscle Milk</h6>
+              </div>
+            </div>
           </div>
         </div>
       </div>
