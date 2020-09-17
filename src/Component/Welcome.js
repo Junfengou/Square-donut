b5812f0ef96d6c88d28fc3baca4b055ee49db3f0
Junfeng ou <junfengou@gmail.com>
2020-09-17 11:59:23 -0500

    Fix up some issues

--- src/Component/Welcome.js
+++ src/Component/Welcome.js
@@ -6,17 +6,19 @@ import tagline from "../pictures/Tagline.png";
 function Welcome() {
   return (
     <div className="welcome">
-      <div className="welcome__pictures">
-        <div className="welcome__logo">
-          <img src={logo} />
-        </div>
+      <div className="overlay">
+        <div className="welcome__pictures">
+          <div className="welcome__logo">
+            <img src={logo} />
+          </div>
 
-        <div className="welcome__tagline">
-          <img src={tagline} />
-        </div>
+          <div className="welcome__tagline">
+            <img src={tagline} />
+          </div>
 
-        <div className="welcome__content">
-          <h3>HANDCRAFTED WITH LOVE IN OMAHA, NE</h3>
+          <div className="welcome__content">
+            <h3>HANDCRAFTED WITH LOVE IN OMAHA, NE</h3>
+          </div>
         </div>
       </div>
     </div>
